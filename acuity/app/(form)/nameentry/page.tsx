5d37c2220ad233ace0b670cb63609a9c2c3aeec9
latenace <77824915+latenace@users.noreply.github.com>
2025-03-15 14:40:30 +1100

    Add form name field and back button

--- acuity/app/(form)/nameentry/page.tsx
+++ acuity/app/(form)/nameentry/page.tsx
@@ -5,6 +5,8 @@ import axios from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { useRouter } from "next/router";
+import Link from "next/link";
+import { ArrowLeft } from "lucide-react";
 
 import {
     Form,
@@ -13,6 +15,7 @@ import {
     FormField,
     FormLabel,
     FormMessage,
+    FormItem
 } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -39,6 +42,11 @@ const onSubmit = (values: z.infer<typeof formSchema>) => {
 
     return (
         <div className="max-w-5xl mx-auto md:items-center md:justify-center h-full p-6"> 
+            <div className="mb-6">
+                <Link href="/" className="flex items-center text-gray-600 hover:text-gray-900">
+                    <ArrowLeft className="h-5 w-5 mr-1" />
+                </Link>
+            </div>
             <h1 className="text-2xl">
                 Name your entry
             </h1>
@@ -50,7 +58,27 @@ const onSubmit = (values: z.infer<typeof formSchema>) => {
                 onSubmit={form.handleSubmit(onSubmit)}
                 className="space-y-8 mt-8"
                 >
-                </form>
+                    <FormField
+                    control={form.control}
+                    name="title"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>
+                                Entry title
+                            </FormLabel>
+                            <FormControl>
+                                <Input
+                                disabled={isSubmitting}
+                                placeholder="e.g. &apos;Journal Entry #12&apos;"
+                                {...field}
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+
+                    </form>
 
             </Form>
         </div>
