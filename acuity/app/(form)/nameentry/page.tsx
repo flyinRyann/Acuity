d8260dc06c45b1aa226c418f2ed8bae6fcc5bfc9
latenace <77824915+latenace@users.noreply.github.com>
2025-03-15 14:18:53 +1100

    Add form

---
+++ acuity/app/(form)/nameentry/page.tsx
@@ -0,0 +1,60 @@
+"use client"
+
+import * as  z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/router";
+
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+    title: z.string().min(1, {
+        message: "Title is required",
+    }),
+});
+
+const NameEntry = () => {
+const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+        title: ""
+    },
+});
+
+const { isSubmitting, isValid } = form.formState;
+
+const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+}
+
+    return (
+        <div className="max-w-5xl mx-auto md:items-center md:justify-center h-full p-6"> 
+            <h1 className="text-2xl">
+                Name your entry
+            </h1>
+            <p className="text-sm text-slate-600">
+            What would you like to name your journal entry? Don&apos;t worry you can change this later!
+            </p>
+            <Form {...form}>
+                <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-8 mt-8"
+                >
+                </form>
+
+            </Form>
+        </div>
+    );
+}
+
+export default NameEntry;
