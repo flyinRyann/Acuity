ed32db4a29e8226229c1205bf414a29b5909b93f
--global <atra0049@student.monash.edu>
2025-03-15 20:20:46 +1100

    make pointer "clickable" when hover over create entry

--- acuity/app/(dashboard)/_components/create-entry-button.tsx
+++ acuity/app/(dashboard)/_components/create-entry-button.tsx
@@ -5,7 +5,7 @@ const CreateEntryButton = () => {
     return ( 
         <div>
             <Link href="/page1-name">
-                <Button className="bg-zinc-800 hover:bg-zinc-700">Create Entry</Button>
+                <Button className="bg-zinc-800 hover:bg-zinc-700 cursor-pointer">Create Entry</Button>
             </Link>
         </div>
      );
