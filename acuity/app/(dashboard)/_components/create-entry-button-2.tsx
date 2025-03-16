63b7155c3e2c83e8d5fb49f7572e58f9bb51e2d2
latenace <77824915+latenace@users.noreply.github.com>
2025-03-16 12:48:50 +1100

    Fix card styling and icons

--- acuity/app/(dashboard)/_components/create-entry-button-2.tsx
+++ acuity/app/(dashboard)/_components/create-entry-button-2.tsx
@@ -1,12 +1,16 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { CirclePlus } from "lucide-react";
 
 const CreateEntryButton2 = () => {
   return (
     <div>
-      <Link href="/page1-name">
-        <Button className="bg-zinc-800 hover:bg-zinc-700 cursor-pointer">Add New Entry</Button>
-      </Link>
+        <Link href="/page1-name">
+            <Button className="bg-zinc-800 hover:bg-zinc-700">
+                <CirclePlus className="mr-1 h-4 w-4" />
+                    Add New Entry
+            </Button>
+        </Link>
     </div>
   );
 }
--- acuity/app/(dashboard)/_components/create-entry-button.tsx
+++ acuity/app/(dashboard)/_components/create-entry-button.tsx
@@ -1,11 +1,15 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { CirclePlus } from "lucide-react";
 
 const CreateEntryButton = () => {
     return ( 
         <div>
             <Link href="/page1-name">
-                <Button className="bg-zinc-800 hover:bg-zinc-700">Create Entry</Button>
+                <Button className="bg-zinc-800 hover:bg-zinc-700">
+                    <CirclePlus className="mr-1 h-4 w-4" />
+                    Create Entry
+                </Button>
             </Link>
         </div>
      );
